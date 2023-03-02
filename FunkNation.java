26f3e6a28b2b5cdd604e978b57df6c9bade489db
Jogo2002 <99942554+Jogo2002@users.noreply.github.com>
2023-03-01 23:41:39 -0600

    Add files via upload

---
+++ FunkNation.java
@@ -0,0 +1,23 @@
+//public class FunkNation {
+//    public static void main(String[] args){
+//        int[] arraySki = {1, 2, 3, 4, 1, 3, 17, 3};
+//        int pairTally = 0;
+//        int pointer = arraySki[0];
+//        int beingCompared = arraySki[0];
+//            for (int i = 0; i< arraySki.length; i++);
+//                if( pointer ==  beingCompared){
+//                    pairTally++;
+//                    beingCompared++;
+//            } else {
+//                    beingCompared++;
+//            }
+//        // for every number in array go through each other number
+//        // if numbers match, add to pairTally
+//        // i++, checked nums + 1
+//        //print pairtally
+//
+//        }
+//        System.out.println(int pairTally);
+//
+//    }
+//
